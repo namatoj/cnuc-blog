aa3ea569c8a19df39de236faafafd1f3a4243c92
namatoj <namatoj@gmail.com>
2023-08-22 00:09:53 +0200

    Draft and strip tags

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addCollection("published", (collection) => {
+    return collection
+      .getFilteredByTags("post")
+      .filter((post) => !post.data.draft);
+  });
 };
